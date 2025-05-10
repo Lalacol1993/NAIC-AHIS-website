66c0bdff4a97603ba26e2dbe2d8784c863922a69
Lalacol1993 <139152399+Lalacol1993@users.noreply.github.com>
2025-05-10 20:45:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
-    build: {
+    exclude: ['lucide-react']
+  },
+  build: {
     rollupOptions: {
       external: ['@ebereplenty/chatbot']
     }
   }
-  },
 });
