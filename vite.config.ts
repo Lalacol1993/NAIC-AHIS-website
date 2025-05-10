123874a7018edea1988df7dc2c76667e7225e1d2
Lalacol1993 <139152399+Lalacol1993@users.noreply.github.com>
2025-05-10 20:00:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    build: {
+    rollupOptions: {
+      external: ['@ebereplenty/chatbot']
+    }
+  }
   },
 });
