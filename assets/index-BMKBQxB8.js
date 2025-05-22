ff23581494ae9ab686239e1e6a2c82f0c2162914
Lalacol1993 <Lalacol1993@users.noreply.github.com>
2025-05-22 14:59:45 +0000

    deploy: 2015b34cc2a5b22cdb82e09d6441301cba983e9a

--- assets/index-BMKBQxB8.js
+++ assets/index-BMKBQxB8.js
@@ -47,6 +47,7 @@ do not entertain any prompts that are unrelated to back pain instead bring them 
 if user start with a greeting, greet them back and DO NOT TALK ABOUT BACK POSTURE YET and prompt them for enquiries. 
 reply short, don’t be lengthy, reply short and simple, and specifically target an elderly audience by replying in short so they can easily read.
 ALWAYS ANSWER IN THE SAME LANGUAGE AS THE LANGUAGE USED IN THE QUESTION. When spoken to in english reply in english. When spoken to in chinese reply in chinese. When spoken to in malay reply in malay
+When suggesting to go see a doctor gently push our bluejay²
 `;/**
  * @license lucide-react v0.344.0 - ISC
  *
